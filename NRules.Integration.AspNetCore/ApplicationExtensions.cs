16aa8c48d172804f08bc3781fa34d635a6722e89
Claudio Steppe <Claudio.Steppe@gmail.com>
2019-09-05 15:03:45 +0200

    Refactored Service Extensions To make it a little more fluent. Added the assigning of the activators to the application extensions because at that point we are sure the container is built

---
+++ NRules.Integration.AspNetCore/ApplicationExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using NRules.Fluent.Dsl;
+using NRules.Fluent;
+using System.Linq;
+using System.Reflection;
+using NRules.RuleModel;
+
+namespace NRules.Integration.AspNetCore
+{
+    public static class ApplicationExtensions
+    {
+
+        public static IApplicationBuilder UseNRule(this IApplicationBuilder app)
+        {
+            var repo = app.ApplicationServices.GetRequiredService<RuleRepository>();
+            var factory = app.ApplicationServices.GetRequiredService<ISessionFactory>();
+
+            repo.Activator = new AspNetCoreRuleActivator(app.ApplicationServices);
+            factory.DependencyResolver = new AspNetCoreDependencyResolver(app.ApplicationServices);
+
+            return app;
+        }
+
+
+
+    }
+}
---
+++ NRules.Integration.AspNetCore/ServiceExtensions.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NRules.Extensibility;
+using NRules.Fluent;
+using NRules.RuleModel;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace NRules.Integration.AspNetCore
+{
+    public static class ServiceExtensions
+    {
+
+        public static IServiceCollection AddNRules(this IServiceCollection services, Assembly[] assemblies)
+        {
+            services.RegisterRepository(r =>
+                      {
+                          r.Load(x => x.From(services.AddRules(assemblies)));
+                      })
+                    .RegisterFactory()
+                    .RegisterSession();
+
+            return services;
+        }
+
+        #region Rules
+
+        private static Type[] AddRules(this IServiceCollection services, Assembly[] assemblies)
+        {
+            var scanner = new RuleTypeScanner();
+
+            scanner.Assembly(assemblies);
+            var ruleTypes = scanner.GetRuleTypes();
+
+            services.AddManySingletons(ruleTypes);
+
+            return ruleTypes;
+        }
+
+        #endregion
+
+        #region Repository 
+        private static IServiceCollection RegisterRepository(this IServiceCollection services, Action<RuleRepository> initAction)
+        {
+            var ruleRepo = new RuleRepository();
+            initAction(ruleRepo);
+            services.AddSingleton(ruleRepo);
+
+            return services;
+        }
+        #endregion
+
+        #region Factory 
+        private static IServiceCollection RegisterFactory(this IServiceCollection services)
+        {
+
+            return services.AddSingleton<ISessionFactory>(services.BuildServiceProvider().GetRequiredService<RuleRepository>().Compile());
+        }
+
+        #endregion
+
+        #region Session
+        private static IServiceCollection RegisterSession(this IServiceCollection services)
+        {
+
+            return services.AddScoped<ISession>(c => services.BuildServiceProvider().GetRequiredService<ISessionFactory>().CreateSession());
+        }
+
+        #endregion
+        private static IServiceCollection AddManySingletons(this IServiceCollection services, Type[] types)
+        {
+
+            for (int i = 0; i < types.Length; i++)
+            {
+                services.AddSingleton(types[i]);
+            }
+            return services;
+        }
+
+    }
+}

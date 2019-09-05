c591818084c7174bfbd1ced46cef64f6de70b6a0
Claudio Steppe <claudios@lawforall.co.za>
2019-09-05 12:25:01 +0200

    Add project files.

---
+++ NRules.Integration.AspNetCore/AspNetCoreDependencyResolver.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NRules.Extensibility;
+using System;
+
+namespace NRules.Integration.AspNetCore
+{
+    public class AspNetCoreDependencyResolver : IDependencyResolver
+    {
+        private readonly IServiceProvider _container;
+
+        public AspNetCoreDependencyResolver(IServiceProvider serviceProvider)
+        {
+            _container = serviceProvider;
+        }
+        public object Resolve(IResolutionContext context, Type serviceType)
+        {
+            return _container.GetRequiredService(serviceType);
+        }
+    }
+}
---
+++ NRules.Integration.AspNetCore/AspNetCoreRuleActivator.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NRules.Fluent;
+using NRules.Fluent.Dsl;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NRules.Integration.AspNetCore
+{
+    public class AspNetCoreRuleActivator : IRuleActivator
+    {
+        private readonly IServiceProvider _container;
+
+        public AspNetCoreRuleActivator(IServiceProvider serviceProvider)
+        {
+            _container = serviceProvider;
+        }
+        public IEnumerable<Rule> Activate(Type type)
+        {
+            var requestedService = _container.GetService(type);
+
+            if (requestedService != null)
+            {
+                var collectionType = typeof(IEnumerable<>).MakeGenericType(type);
+                return (IEnumerable<Rule>)_container.GetRequiredService(collectionType);
+            }
+
+            return ActivateDefault(type);
+        }
+
+        private static IEnumerable<Rule> ActivateDefault(Type type)
+        {
+            yield return (Rule)Activator.CreateInstance(type);
+        }
+    }
+}
